b4a42acc8caf3b5af30094c7e572ac72ff0d76b8
inderpal6785 <inderpal6785@gmail.com>
2018-02-08 18:26:29 +0530

    Updated bundle

--- demosite/config/initializers/oxd_config.rb
+++ demosite/config/initializers/oxd_config.rb
@@ -2,12 +2,12 @@
 Oxd.configure do |config|
   	config.oxd_host_ip                			= '127.0.0.1'
 	config.oxd_host_port      		  			= 8099
-	config.authorization_redirect_uri 			= "https://oxd-rails.com/login"
-	config.logout_redirect_uri 		  			= "https://oxd-rails.com/logout"
-	config.post_logout_redirect_uri	  			= "https://oxd-rails.com/logout"
+	config.authorization_redirect_uri 			= "https://oxd-ruby.com/login"
+	config.logout_redirect_uri 		  			= "https://oxd-ruby.com/logout"
+	config.post_logout_redirect_uri	  			= "https://oxd-ruby.com"
 	config.scope					  			= [ "openid", "profile" ]
 	config.application_type       	  			= "web"
-	config.redirect_uris     		  			= ["https://oxd-rails.com/login" ]
+	config.redirect_uris     		  			= ["https://oxd-ruby.com/login" ]
 	config.client_jwks_uri			  			= ""
 	config.client_token_endpoint_auth_method	= ""
 	config.client_request_uris					= []
@@ -15,5 +15,5 @@ Oxd.configure do |config|
 	config.grant_types							= []
 	config.response_types     		  			= ["code"]
 	config.acr_values 				  			= ["basic"]
-	config.client_logout_uris		  			= ['https://oxd-rails.com/logout']
+	config.client_logout_uris		  			= ['https://oxd-ruby.com/logout']
 end
